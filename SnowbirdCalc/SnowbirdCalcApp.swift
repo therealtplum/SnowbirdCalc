f5d4105c44db5a88215ae3920935a230392f35b0
therealtplum <32345139+therealtplum@users.noreply.github.com>
2025-09-29 19:55:03 -0500

    Initial Commit

---
+++ SnowbirdCalc/SnowbirdCalcApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowbirdCalcApp.swift
+//  SnowbirdCalc
+//
+//  Created by Thomas Plummer on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct SnowbirdCalcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnowbirdCalcTests/SnowbirdCalcTests.swift
@@ -0,0 +1,16 @@
+//
+//  SnowbirdCalcTests.swift
+//  SnowbirdCalcTests
+//
+//  Created by Thomas Plummer on 9/29/25.
+//
+
+import Testing
+
+struct SnowbirdCalcTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
